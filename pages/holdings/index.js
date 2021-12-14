dd928a3e63bbc5a6561d422dd47c4894939438c7
Xinchen Liu <mike000708@me.com>
2021-12-14 16:53:30 -0500

    added holdings page

---
+++ pages/holdings/index.js
@@ -0,0 +1,26 @@
+import {
+  Box,
+  Stack,
+  StackProps,
+  useColorModeValue as mode,
+} from "@chakra-ui/react";
+import PageContainer from "../../components/PageContainer/PageContainer";
+import { Card } from "../../components/Card/Card";
+import { HeadingGroup } from "../../components/Forms/HeadingGroup";
+
+export default function Page() {
+  let props;
+
+  return (
+    <PageContainer>
+      <Box bg={mode("purple.50", "purple.800")} py="10">
+        <Box mx="auto" w="90%">
+          <Stack as="section" spacing="6" {...props}>
+            <HeadingGroup title="Holdings" size="lg" />
+            <Card></Card>
+          </Stack>
+        </Box>
+      </Box>
+    </PageContainer>
+  );
+}
