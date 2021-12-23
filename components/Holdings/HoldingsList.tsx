9e6c747cbd95dfd59781436f3335b49e15b1e637
Xinchen Liu <mike000708@me.com>
2021-12-22 21:17:47 -0500

    added dynamic routing with query

--- components/Holdings/HoldingsList.tsx
+++ components/Holdings/HoldingsList.tsx
@@ -16,9 +16,12 @@ import { useWeb3React } from "@web3-react/core";
 import { useZapper } from "../../hooks/useZapper";
 import { ZAPPER_NETWORK } from "../../utils/constants";
 
-export default function HoldingsList() {
+export default function HoldingsList(props) {
   const { library, account, chainId } = useWeb3React();
-  const walletData = useZapper(account, ZAPPER_NETWORK);
+  const walletData = useZapper(
+    props.accountAddress ? props.accountAddress : account,
+    props.chain ? props.chain : ZAPPER_NETWORK
+  );
 
   return (
     <Box as="section" py={{ base: "2", md: "4" }}>
