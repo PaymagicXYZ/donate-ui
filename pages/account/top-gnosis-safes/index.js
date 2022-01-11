27f4e4dc6c65c6c47ed5474271ec95ff44d4e7ba
Xinchen Liu <mike000708@me.com>
2022-01-11 11:58:33 -0500

    add ens support

---
+++ pages/account/top-gnosis-safes/index.js
@@ -0,0 +1,28 @@
+import {
+  Box,
+  Stack,
+  StackProps,
+  useColorModeValue as mode,
+} from "@chakra-ui/react";
+import PageContainer from "../../../components/PageContainer/PageContainer";
+import { Card } from "../../../components/Card/Card";
+import { HeadingGroup } from "../../../components/Forms/HeadingGroup";
+
+export default function Page() {
+  let props;
+
+  return (
+    <PageContainer>
+      <Box bg={mode("purple.50", "purple.800")} py="10">
+        <Box mx="auto" w="90%">
+          <Stack as="section" spacing="6" {...props}>
+            <HeadingGroup title="Holdings" size="lg" />
+            <Card>
+              <iframe={`https://dune.xyz/embeds/1171/1994/2f9d12ee-3981-4de1-a79d-f8efba29f238`} />
+            </Card>
+          </Stack>
+        </Box>
+      </Box>
+    </PageContainer>
+  );
+}
--- pages/holdings/[chain]/[accountAddress].js
+++ pages/holdings/[chain]/[accountAddress].js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import {
   Box,
   Stack,
@@ -7,6 +8,7 @@ import {
   Text,
   useColorModeValue as mode,
 } from "@chakra-ui/react";
+import { ethers } from "ethers";
 import PageContainer from "../../../components/PageContainer/PageContainer";
 import { Card } from "../../../components/Card/Card";
 import { HeadingGroup } from "../../../components/Forms/HeadingGroup";
@@ -15,19 +17,39 @@ import HoldingsList from "../../../components/Holdings/HoldingsList";
 export default function Page() {
   let props;
   const router = useRouter();
+  const [ENSname, setENSname] = useState();
   const { chain, accountAddress } = router.query;
+  const validAddress = new RegExp(/^0x[a-fA-F0-9]{40}$/);
+
+  useEffect(() => {
+    if (chain === "ethereum") {
+      const provider = ethers.getDefaultProvider();
+      if (validAddress.test(accountAddress)) {
+        const getENSname = async () => {
+          try {
+            setENSname(await provider.lookupAddress(accountAddress));
+          } catch (err) {
+            console.log(err);
+          }
+        };
+        getENSname();
+      }
+    } else if
+  });
   return (
     <PageContainer>
       <Box bg={mode("purple.50", "purple.800")} py="10">
         <Box mx="auto" w="90%">
           <Stack as="section" spacing="6" {...props}>
             <HeadingGroup
-              title={`Holdings for "${accountAddress}" on ${chain}`}
+              title={`Holdings for "${
+                ENSname ? ENSname : accountAddress
+              }" on ${chain}`}
               size="lg"
             />
             <Text>
               Please be aware, this might not be your account. &nbsp;
-              <Link to="/holdings" color="teal.500">
+              <Link href="/holdings" color="teal.500">
                 To your account
               </Link>
             </Text>
