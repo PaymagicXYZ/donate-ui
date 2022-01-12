6bea3762950a768b0c7105a42cacdb99c0ab93c0
Xinchen Liu <mike000708@me.com>
2022-01-12 09:21:55 -0500

    add regex and unstoppabledomain support

--- pages/holdings/[chain]/[accountAddress].js
+++ pages/holdings/[chain]/[accountAddress].js
@@ -13,29 +13,71 @@ import PageContainer from "../../../components/PageContainer/PageContainer";
 import { Card } from "../../../components/Card/Card";
 import { HeadingGroup } from "../../../components/Forms/HeadingGroup";
 import HoldingsList from "../../../components/Holdings/HoldingsList";
+const { default: Resolution } = require("@unstoppabledomains/resolution");
 
 export default function Page() {
   let props;
   const router = useRouter();
-  const [ENSname, setENSname] = useState();
+  const [Altname, setAltname] = useState();
+  const [address, setAddress] = useState();
   const { chain, accountAddress } = router.query;
   const validAddress = new RegExp(/^0x[a-fA-F0-9]{40}$/);
+  const validETHAddress = new RegExp(/(\.?(eth)$)/);
+  const validUDAddress = new RegExp(
+    /(\.?(zil|crypto|nft|blockchain|bitcoin|coin|wallet|888|dao|x)$)/
+  );
+  const provider = ethers.getDefaultProvider();
+  const resolution = new Resolution();
 
   useEffect(() => {
-    if (chain === "ethereum") {
-      const provider = ethers.getDefaultProvider();
-      if (validAddress.test(accountAddress)) {
+    if (validAddress.test(accountAddress)) {
+      if (chain === "ethereum") {
         const getENSname = async () => {
           try {
-            setENSname(await provider.lookupAddress(accountAddress));
+            setAltname(await provider.lookupAddress(accountAddress));
           } catch (err) {
             console.log(err);
           }
         };
         getENSname();
       }
-    } else if
+    }
+    if (validETHAddress.test(accountAddress) && chain === "ethereum") {
+      const getENSresolve = async () => {
+        try {
+          const resolver = await provider.getResolver(accountAddress);
+          setAddress(await resolver.getAddress());
+          console.log(address);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      getENSresolve();
+    }
+
+    if (validUDAddress.test(accountAddress)) {
+      function resolve(domain, currency) {
+        resolution
+          .addr(domain, currency)
+          .then((address) => console.log(domain, "resolves to", address))
+          .catch(console.error);
+      }
+      const getUDresolve = async (coin) => {
+        try {
+          setAddress(resolve(accountAddress, coin));
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      if (chain === "ethereum") {
+        getUDresolve("ETH");
+      }
+      if (chain === "polygon") {
+        getUDresolve("MATIC");
+      }
+    }
   });
+
   return (
     <PageContainer>
       <Box bg={mode("purple.50", "purple.800")} py="10">
@@ -43,7 +85,7 @@ export default function Page() {
           <Stack as="section" spacing="6" {...props}>
             <HeadingGroup
               title={`Holdings for "${
-                ENSname ? ENSname : accountAddress
+                Altname ? Altname : accountAddress
               }" on ${chain}`}
               size="lg"
             />
@@ -55,7 +97,10 @@ export default function Page() {
             </Text>
 
             <Card>
-              <HoldingsList chain={chain} accountAddress={accountAddress} />
+              <HoldingsList
+                chain={chain}
+                accountAddress={address ? address : accountAddress}
+              />
             </Card>
           </Stack>
         </Box>
