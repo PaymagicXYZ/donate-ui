0ea71845320b8ce0ca98c6510de22d2e7b3e4fb9
Xinchen Liu <mike000708@me.com>
2022-01-17 10:50:09 -0500

    update to new logo

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -1,28 +1,15 @@
 import Link from "next/link";
-import {
-  Heading,
-  Image,
-  Stack,
-} from "@chakra-ui/react";
-import * as React from 'react'
+import { Heading, Image, Stack } from "@chakra-ui/react";
+import * as React from "react";
 
 export const Logo = () => {
   return (
-        <Link href="/">
-          <a>
-            <Stack direction="row" alignItems="center">
-              <Image
-                src="/logo_512x512.png"
-                alt="Paymagic"
-                boxSize="48px"
-                objectFit="cover"
-                borderRadius="50%"
-              />
-              <Heading as="h1" size="lg" isTruncated>
-                Paymagic
-              </Heading>
-            </Stack>
-          </a>
-        </Link>
-  )
-}
+    <Link href="/">
+      <a>
+        <Stack direction="row" alignItems="center">
+          <Image src="/logo.png" alt="Paymagic" maxWidth="25vw" width="220px" />
+        </Stack>
+      </a>
+    </Link>
+  );
+};
