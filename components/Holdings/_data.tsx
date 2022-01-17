6bd7718b2b8a431ee0affb4dbc1d189df7f441a2
Xinchen Liu <mike000708@me.com>
2022-01-17 11:27:37 -0500

    Use TokenAmountDisplay and TokenDisplay

--- components/Holdings/_data.tsx
+++ components/Holdings/_data.tsx
@@ -2,24 +2,31 @@ import { Badge } from "@chakra-ui/react";
 import * as React from "react";
 import { Text, Link, Image } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
-
 import { shortenAddress } from "../../utils";
+import TokenDisplay from "../TokenDisplay";
+import TokenAmountDisplay from "../TokenAmountDisplay";
 
 export const cols = [
   {
     Header: "Symbol",
     accessor: "symbol",
     Cell: function AssetCell(data: any) {
-      return data;
+      return <TokenDisplay symbol={data} />;
     },
   },
   {
     Header: "Balance",
     accessor: "balance",
+    Cell: function AmountCell(data: any) {
+      return <TokenAmountDisplay amountTokens={data} />;
+    },
   },
   {
     Header: "Value in USD",
     accessor: "balanceUSD",
+    Cell: function AmountCell(data: any) {
+      return <TokenAmountDisplay amountTokens={data} />;
+    },
   },
   {
     Header: "Contract",
