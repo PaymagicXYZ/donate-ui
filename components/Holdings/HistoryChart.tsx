1e2f01676db52ff804b9af577a79bcf03b49b2da
Xinchen Liu <mike000708@me.com>
2022-01-22 00:12:21 -0500

    graph for value

--- components/Holdings/HistoryChart.tsx
+++ components/Holdings/HistoryChart.tsx
@@ -10,6 +10,7 @@ import {
 } from "recharts";
 import _ from "lodash";
 import { Center, Container, Text, Select } from "@chakra-ui/react";
+// import numeral from "numeral";
 
 export function HistoryChart(props) {
   const { covalentData } = props;
@@ -24,29 +25,36 @@ export function HistoryChart(props) {
   }
   const dates = [];
   const value = [];
-  const data = {};
+  if (covalentData.loading == false) {
+    covalentData.history.items[0].holdings.map((item) => {
+      dates.unshift(item.timestamp);
+    });
+    for (let i = 0; i < 31; i++) {
+      let calculatedValue = 0;
+      covalentData.history.items.forEach((item) => {
+        calculatedValue += item.holdings[i].close.quote;
+      });
+      value.unshift(calculatedValue);
+    }
+  }
 
   useEffect(() => {
-    setActiveDates(dates.slice(dates.length - days));
-    for (let i = 0; i < days; i++) {
-      let totalValue = 0;
-      covalentData.history.items.forEach((item) => {
-        totalValue += item.holdings[i].close.quote;
-      });
-      value.unshift(totalValue);
+    if (
+      covalentData.loading == false &&
+      covalentData.history.items.length > 0
+    ) {
+      setActiveDates(
+        dates.slice(dates.length - days).map((date) => {
+          return getFormattedDate(new Date(date));
+        })
+      );
+      setTotalValue(value.slice(dates.length - days));
+      // .map((value) => `${numeral(value).format("$0,00")}`)
     }
-    setTotalValue(value);
   }, [days]);
 
   return (
     <Container>
-      <Select onChange={(e) => setDays(Number(e.target.value))}>
-        <option value={30} selected>
-          Last 30 Days
-        </option>
-        <option value={15}>Last 15 Days</option>
-        <option value={7}>Last 7 Days</option>
-      </Select>
       {covalentData.loading ? (
         <Center p={6}>
           <Text as="i">Fetching data</Text>
@@ -56,45 +64,43 @@ export function HistoryChart(props) {
           <Text as="i">No history found</Text>
         </Center>
       ) : (
-        <pre>
-          {covalentData.history.items[0].holdings.map((item) => {
-            dates.unshift(item.timestamp);
-          })}
-          <code>
-            {JSON.stringify(
-              {
-                activeDates,
-                totalValue,
-              },
-              null,
-              2
-            )}
-          </code>
-        </pre>
-        // <ResponsiveContainer width="100%" height="100%">
-        //   <AreaChart
-        //     width={500}
-        //     height={400}
-        //     data={data}
-        //     margin={{
-        //       top: 10,
-        //       right: 30,
-        //       left: 0,
-        //       bottom: 0,
-        //     }}
-        //   >
-        //     <CartesianGrid strokeDasharray="3 3" />
-        //     <XAxis dataKey="name" />
-        //     <YAxis />
-        //     <Tooltip />
-        //     <Area
-        //       type="monotone"
-        //       dataKey="uv"
-        //       stroke="#8884d8"
-        //       fill="#8884d8"
-        //     />
-        //   </AreaChart>
-        // </ResponsiveContainer>
+        <>
+          {/* <pre>
+            <code>{JSON.stringify(activeDates, null, 2)}</code>
+            <code>{JSON.stringify(totalValue, null, 2)}</code>
+          </pre> */}
+          <AreaChart
+            width={500}
+            height={400}
+            data={activeDates.map((date, i) => {
+              return { name: date, value: totalValue[i] };
+            })}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="value"
+              stroke="#8884d8"
+              fill="#8884d8"
+            />
+          </AreaChart>
+          <Select onChange={(e) => setDays(Number(e.target.value))}>
+            <option value={30} selected>
+              Last 30 Days
+            </option>
+            <option value={15}>Last 15 Days</option>
+            <option value={7}>Last 7 Days</option>
+          </Select>
+        </>
       )}
     </Container>
   );
