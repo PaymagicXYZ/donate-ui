afdeaa2726f318f1c367c1843eb26da30bbb87f8
Xinchen Liu <mike000708@me.com>
2022-02-02 09:43:05 -0500

    urql for gnosis subgraph

--- hooks/useSubgraph.js
+++ hooks/useSubgraph.js
@@ -1,29 +1,35 @@
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
-
-// const APIURL = 'https://api.thegraph.com/subgraphs/name/gjeanmart/gnosis-safe-mainnet';
+import { useEffect, useState } from "react";
+import _ from "lodash";
+import { createClient } from "urql";
 
 const APIURL =
-  "https://api.studio.thegraph.com/query//gjeanmart/gnosis-safe-mainnet/";
+  "https://api.thegraph.com/subgraphs/name/gjeanmart/gnosis-safe-mainnet";
+
+const client = createClient({
+  url: APIURL,
+});
+
+// const APIURL =
+//   "https://api.studio.thegraph.com/query/gjeanmart/gnosis-safe-mainnet/";
 
 const tokensQuery = `
   query {
-     wallets {
+  wallet(id: "0x8e4d87ad4dafdac06d86a3ed002f5085dc036e08") {
     id
+    creator
+    network
     stamp
+    hash
+    factory
+    mastercopy
     owners
     threshold
-    creator
-    factory
-    version
     currentNonce
+    version
   }
-  }
+}
 `;
-
-const client = new ApolloClient({
-  uri: APIURL,
-  cache: new InMemoryCache(),
-});
 
 // function getSafeQuery(id)=>{return `query {
 //       wallets(id: "${id}") {
@@ -46,18 +52,11 @@ export function useSubgraph(address) {
     subgraph: null,
   });
   useEffect(() => {
-    async function getData(address) {
-      client
-        .query({
-          query: gql(tokensQuery),
-        })
-        .then((data) => {
-          setData({ loading: false, subgraph: data });
-          console.log("Subgraph data: ", data);
-        })
-        .catch((err) => {
-          console.log("Error fetching data: ", err);
-        });
+    async function getData() {
+      const data = await client.query(tokensQuery).toPromise();
+      console.log(data);
+      setData({ loading: false, subgraph: data });
+      // console.log("Subgraph data: ", data);
     }
 
     if (!_.isUndefined(address)) {
--- pages/accounts/top-gnosis-safes/index.js
+++ pages/accounts/top-gnosis-safes/index.js
@@ -1,26 +1,66 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import {
   Box,
   Stack,
   StackProps,
+  Link,
+  Text,
+  Image,
   useColorModeValue as mode,
 } from "@chakra-ui/react";
 import PageContainer from "../../../components/PageContainer/PageContainer";
 import { Card } from "../../../components/Card/Card";
 import { HeadingGroup } from "../../../components/Forms/HeadingGroup";
+import Topsafes from "../../../components/Safes/Topsafes";
+import { createClient } from "urql";
 
 export default function Page() {
   let props;
+  const [graphData, setGraphData] = useState({
+    loading: true,
+    subgraph: null,
+  });
+  const APIURL =
+    "https://api.thegraph.com/subgraphs/name/gjeanmart/gnosis-safe-mainnet";
+
+  const client = createClient({
+    url: APIURL,
+  });
+
+  const tokensQuery = `
+  query {
+  wallets {
+    id
+    stamp
+    owners
+    threshold
+    creator
+    factory
+    version
+    currentNonce
+  }
+}
+`;
+  useEffect(() => {
+    fetchTopSafes();
+  }, []);
+
+  async function fetchTopSafes() {
+    const data = await client.query(tokensQuery).toPromise();
+    console.log(data);
+    setGraphData({ loading: false, subgraph: data });
+  }
 
   return (
     <PageContainer>
-      <Box bg={mode("purple.50", "purple.800")} py="10">
+      <Box bg={mode("purple.50", "purple.800")}>
         <Box mx="auto" w="90%">
-          <Stack as="section" spacing="6" {...props}>
-            <HeadingGroup title="Holdings" size="lg" />
-            <Card>
-              {/* <iframe={`https://dune.xyz/embeds/1171/1994/2f9d12ee-3981-4de1-a79d-f8efba29f238`} /> */}
-            </Card>
-          </Stack>
+          <Card>
+            <pre>
+              <code>{JSON.stringify(graphData, null, 2)}</code>
+            </pre>
+          </Card>
         </Box>
       </Box>
     </PageContainer>
