c57afaabbbdb2109e2d4529906427d40529d060d
Corbin Page <corbin.page@gmail.com>
2022-02-17 14:49:44 -0700

    Troubleshooting deployment

--- pages/dustsweeper/index.js
+++ pages/dustsweeper/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import {
+  Heading,
   Text,
   Box,
   Center,
@@ -9,25 +10,37 @@ import {
   TabPanel,
   TabPanels,
   VStack,
+  HStack,
+  Stack,
+  OrderedList,
+  ListItem,
+  useColorModeValue as mode,
 } from "@chakra-ui/react";
 import SubmitApprovalsForm from '../../components/DustSweeper/SubmitApprovalsForm'
 import ApprovalTable from '../../components/DustSweeper/ApprovalTable'
 import PageContainer from "../../components/PageContainer/PageContainer";
 import { HeadingGroup } from "../../components/Forms/HeadingGroup";
-import * as tokens from './tokens.json'
 
 export default function Page() {
   return (
     <PageContainer>
-      <Box w={['97%', '90%', '60%']} mx="auto" >
-        <VStack>
-          <Center>
-            <HeadingGroup title="[OLD] Dust Sweeper  🧹" size="lg" />
-          </Center>
-          <VStack>
-            <Text>🧹  Clean out small token balances in your wallet without paying gas!</Text>
-            <Text>👆  Connect your wallet and approve tokens (costing $8-15 each) to get rid off.</Text>
-            <Text>🤝  ETH will appear in your wallet once a bot pays gas to fill the trade.</Text>
+      <Box w={['97%', '90%', '70%']} mx="auto" >
+        <VStack mt="8" mb="3">
+          <Heading as="h3" size="md">
+            🧹  DustSweeper AMM
+          </Heading>
+          <Text size='md' >
+            Clean small token balances from your wallet and swap for ETH!
+          </Text>
+          <VStack mt='8'>
+            <Heading as="h5" size="sm" mt='8' align='right'>
+              How to use:
+            </Heading>
+            <OrderedList>
+              <ListItem fontSize="md">✅  Approve tokens (costing $8-15 each) to swap.</ListItem>
+              <ListItem fontSize="md">🤖  Bots will execute the swap and pay the gas.</ListItem>
+              <ListItem fontSize="md">💰  You'll receive ETH in your wallet. Done!</ListItem>
+            </OrderedList>
           </VStack>
         </VStack>
         <Tabs variant='soft-rounded' colorScheme='purple' mt="8">
