81933fa1ecc2072532c0a010aa902a76bcbeab16
Xinchen Liu <mike000708@me.com>
2022-02-28 16:09:24 -0500

    add multiwallet support

--- components/Header/Wallet.tsx
+++ components/Header/Wallet.tsx
@@ -3,13 +3,6 @@ import {
   Button,
   ButtonGroup,
   Spacer,
-  Menu,
-  MenuList,
-  MenuItem,
-  MenuButton,
-  Box,
-  Flex,
-  Text,
   HStack,
   Modal,
   ModalOverlay,
@@ -26,7 +19,7 @@ import { useWeb3React } from "@web3-react/core";
 // import { BigNumber } from "@ethersproject/bignumber";
 import { useEagerConnect } from "../../hooks/useEagerConnect";
 import { useInactiveListener } from "../../hooks/useInactiveListener";
-import { injected } from "../../connectors";
+import { injected, walletconnect, walletlink } from "../../connectors";
 // import { translateChainId } from "../../utils";
 import NetworkMenu from "./NetworkMenu";
 import { MoreItems } from "./MoreItems";
@@ -104,8 +97,34 @@ export default function Wallet() {
               <ModalHeader>Please select a wallet to connect to</ModalHeader>
               <ModalCloseButton />
               <ModalBody>
-                <Button colorScheme="purple" onClick={() => activate(injected)}>
-                  Metamask
+                <Button
+                  width="100%"
+                  height="55px"
+                  colorScheme="yellow"
+                  onClick={() => activate(injected)}
+                >
+                  <Avatar src="/metamask.jpeg" />
+                  &nbsp;Metamask
+                </Button>
+                <br />
+                <Button
+                  width="100%"
+                  height="55px"
+                  colorScheme="teal"
+                  onClick={() => activate(walletconnect)}
+                >
+                  <Avatar src="/walletconnect.png" />
+                  &nbsp;WalletConnect
+                </Button>
+                <br />
+                <Button
+                  width="100%"
+                  height="55px"
+                  colorScheme="blue"
+                  onClick={() => activate(walletlink)}
+                >
+                  <Avatar src="/walletlink.svg" />
+                  &nbsp;Walletlink
                 </Button>
               </ModalBody>
 
