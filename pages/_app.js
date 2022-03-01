783e0b9306ca53599c79e63b6166717a1633dd79
Corbin Page <corbin.page@gmail.com>
2022-02-28 22:18:54 -0500

    Troubleshooting deployment

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { Web3Provider } from "@ethersproject/providers";
 import { theme } from "../styles/theme"
 import ReactGA from 'react-ga';
 
-ReactGA.initialize(' UA-44648226-14 ');
+ReactGA.initialize('UA-44648226-14');
 
 function getLibrary(provider) {
   const library = new Web3Provider(provider);
