c90127549014e84ce2dd411bd29e037d9cdb3ab2
Corbin Page <corbin.page@gmail.com>
2022-03-02 12:35:02 -0500

    Troubleshooting deployment

--- pages/_app.js
+++ pages/_app.js
@@ -19,20 +19,19 @@ function MyApp({ Component, pageProps }) {
           <title>DustSweeper</title>
           <meta name="description" content="Fill in" />
           <link rel="icon" href="/favicon.ico" />
-
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-              ga('create', 'UA-44648226-14', 'auto');
-              ga('send', 'pageview');
-            `}
-          </Script>
-          
         </Head>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-44648226-14', 'auto');
+            ga('send', 'pageview');
+          `}
+        </Script>
+        
         <Component {...pageProps} />
       </Web3ReactProvider>
     </ChakraProvider>
