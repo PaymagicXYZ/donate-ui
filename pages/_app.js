51c973380264c1696314940d158254445b4dfbb1
Corbin Page <corbin.page@gmail.com>
2022-03-02 11:50:58 -0500

    Troubleshooting GA

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,7 @@ import Head from "next/head";
 import { Web3ReactProvider } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
 import { theme } from "../styles/theme"
-import ReactGA from 'react-ga';
-
-ReactGA.initialize('UA-44648226-14');
+import Script from 'next/script'
 
 function getLibrary(provider) {
   const library = new Web3Provider(provider);
@@ -21,6 +19,19 @@ function MyApp({ Component, pageProps }) {
           <title>DustSweeper</title>
           <meta name="description" content="Fill in" />
           <link rel="icon" href="/favicon.ico" />
+
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+              ga('create', 'UA-44648226-14', 'auto');
+              ga('send', 'pageview');
+            `}
+          </Script>
+          
         </Head>
         <Component {...pageProps} />
       </Web3ReactProvider>
