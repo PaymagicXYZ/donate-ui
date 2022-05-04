c1258b7645ff98e6aeac0f91775b9b08be6a494c
Sam Wellander <samwellander@gmail.com>
2022-05-04 08:53:37 -0700

    feat: update logo text

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Heading, Image, Stack, Text } from "@chakra-ui/react";
+import { Stack, Text } from "@chakra-ui/react";
 import * as React from "react";
 
 export const Logo = () => {
@@ -7,7 +7,7 @@ export const Logo = () => {
     <Link href="/">
       <a>
         <Stack direction="row" alignItems="center">
-          <Text letterSpacing={3}>EthereumSupports.me</Text>
+          <Text letterSpacing={3}>EthGives.to</Text>
         </Stack>
       </a>
     </Link>
