1568188fb3c5b3c568d3ec81622f1a0f1e857765
Sam Wellander <samwellander@gmail.com>
2022-05-04 13:02:16 -0700

    feat: finish first draft of cause banner

--- components/DonationForm/CauseBanner.tsx
+++ components/DonationForm/CauseBanner.tsx
@@ -1,16 +1,39 @@
-import { VStack, Box, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Grid,
+  GridItem,
+  VStack,
+  Text,
+  HStack,
+  Button,
+} from "@chakra-ui/react";
+
+const LEARN_MORE_LINK =
+  "https://twitter.com/fcmartinelli/status/1519609111935127553?s=21&t=h8tZQnOqyFTZtbpqiYILfg";
 
 export default () => (
   <VStack w="full" p="3">
-    <Box borderRadius={25} backgroundColor="purple.100" w="full" py="20px">
-      <VStack alignItems="flex-start">
-        <Text px="20px" fontSize="lg">
-          Donate to Stani
-        </Text>
-        <Text px="20px" fontSize="sm" my={0}>
-          Help Stani Kulechov regain his Twitter voice!
-        </Text>
-      </VStack>
-    </Box>
+    <HStack borderRadius={25} backgroundColor="purple.100" w="full" py="20px">
+      <Grid templateColumns="repeat(3, 1fr)" gap={0}>
+        <GridItem colSpan={2}>
+          <VStack alignItems="flex-start">
+            <Text px="20px" fontSize="lg">
+              Donate to Stani
+            </Text>
+            <Text lineHeight="15px" px="20px" fontSize="xs" my={0}>
+              Help Stani Kulechov regain his Twitter voice! and then some more
+              text yahooo
+            </Text>
+          </VStack>
+        </GridItem>
+        <Flex alignItems="center" justify="center">
+          <Button borderRadius="10px">
+            <a target="_blank" href={LEARN_MORE_LINK}>
+              Learn More
+            </a>
+          </Button>
+        </Flex>
+      </Grid>
+    </HStack>
   </VStack>
 );
